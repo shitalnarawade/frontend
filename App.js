c379d36fe1d3faf43dea088683d0607f96f087ce
shitalnarawade <153253941+shitalnarawade@users.noreply.github.com>
2024-03-02 23:01:05 +0530

    first react pro

---
+++ App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenceItems from './components/ExpenceItems';
+
+function App() {
+  return (
+    
+     <div className="App">
+      
+    {  <header className="App-header"> 
+    <ExpenceItems></ExpenceItems>
+    {/* //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <ExpenceItems></ExpenceItems>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a> */}
+     </header> }
+     </div>
+  );
+}
+
+export default App;
---
+++ ExpenceItems.js
@@ -0,0 +1,12 @@
+function ExpenceItems() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+      <div>Amount</div>
+      </div>
+      </div>
+  );
+}
+export default ExpenceItems;
