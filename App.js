b74121319f1c351760f36b44cc2b02acb97e793f
shitalnarawade <153253941+shitalnarawade@users.noreply.github.com>
2024-03-04 18:47:54 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,27 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 import ExpenceItems from './components/ExpenceItems';
 
+
 function App() {
+  const expences = [
+    {
+      id : 'e1',title : 'Toilet Paper', amount: 125.90, date : new Date(2024, 2, 1), city:'Mumbai'},
+    {
+      id : 'e2',title : 'Some Thing new', amount: 100.90, date : new Date(2024, 2, 2), city:'Pune'},
+    {
+      id : 'e3',title : 'Car Insurance', amount: 180.90, date : new Date(2024, 2, 3), city:'Sangali'},
+    {
+      id : 'e4',title : 'New Desk', amount: 290.90, date : new Date(2024, 2, 4),city:'Akola'},
+ ]
   return (
     
-     <div className="App">
-      
-    {  <header className="App-header"> 
-    <ExpenceItems></ExpenceItems>
-    {/* //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <ExpenceItems></ExpenceItems>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a> */}
-     </header> }
+  <div className ="App">
+      {expences.map((expence, index) => (
+        <ExpenceItems
+          key={index}
+          title={expence.title}
+          amount={expence.amount}
+          date={expence.date}
+          city={expence.city}
+        />
+      ))}
+   
+    {/* <ExpenceItems 
+    title={expence[0].title}
+    amount={expence[0].amount} 
+    date={expence[0].date}
+    city ={expence[0].city}
+    ></ExpenceItems>
+    <ExpenceItems
+    title={expence[1].title}
+    amount={expence[1].amount} 
+    date={expence[1].date}
+    city ={expence[1].city}
+    ></ExpenceItems>
+    <ExpenceItems
+    title={expence[2].title}
+    amount={expence[2].amount} 
+    date={expence[2].date}
+    city ={expence[2].city}
+    ></ExpenceItems>
+    <ExpenceItems
+    title={expence[3].title}
+    amount={expence[3].amount} 
+    date={expence[3].date}
+    city ={expence[3].city}
+    ></ExpenceItems> */}
      </div>
   );
 }
--- ExpenceItems.js
+++ ExpenceItems.js
@@ -1,13 +1,18 @@
 import './ExpenceItems.css'
 
-function ExpenceItems() {
+function ExpenceItems(props) {
+    // const expanceDate = new Date(2024, 2 ,4);
+    // const expanceTitle = 'Car Insurance';
+    // const expenceAmount = '$100.90';
+    // const LocationOfExpenditure = 'Akola';
   return (
     <div className='expense-item'>
-      <div>March 4 2024</div>
-      <div>
-        <h2 className='expense-item__description'>Car Insurance</h2>
-      <div className='expense-item__price'>$100.90</div>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+      <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <div className='expense-item__city'>{props.city}</div>
       </div>
   );
 }
